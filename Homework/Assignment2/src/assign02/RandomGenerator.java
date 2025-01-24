308405280a355e11c5a3cc39239195374b90f5da
kjmorse2 <kenmorse2@proton.me>
2025-01-23 18:19:34 -0700

    working more on ass 3, making custon object for grades

--- Homework/Assignment2/src/assign02/RandomGenerator.java
+++ Homework/Assignment2/src/assign02/RandomGenerator.java
@@ -15,19 +15,62 @@ public class RandomGenerator {
 		
 		while(true) {
 			
-			String name = getName(input);
-			
+			System.out.println("What is Your Name?: ");
+			String name = input.nextLine(); //  scans whole line for all text input
+			if(name.equals("")) {
+				System.out.println("Name must be at least one character long, try again"); // not sure if none is valid name, but assignment 
+				continue; 																	// says "After typing the name, the user will press enter."
+																							// so name is not empty?
+			}
 			if(name.equals("exit")) { // handles exit case
 				break;
 			}
 			
-			int sumOfChars = sumOfChars(name);
 			
-			int[] luckyNums = luckyNumGenerator(sumOfChars);
+			int sumOfChars = 0;
 			
-			String luckStatus = areTheyLucky(luckyNums);
+			for(int i = 0; i < name.length(); i++) {
+				sumOfChars += name.charAt(i); // sums numerical values of chars together
+			}
 			
-			printAnswer(luckyNums, luckStatus);
+			
+			int[] luckyNums = new int[5];
+			int place = 0;
+			
+			while(place < 5) {
+				sumOfChars = (73 * sumOfChars + 37) % 128;
+				if(sumOfChars <= 100) { //  ignores numbers over 100 and continues to next iteration of loop
+					luckyNums[place] = sumOfChars; // uses while loop to ensure places reaches 4
+					place += 1;
+				}
+			}
+			
+			
+			int luckySum = 0; // determines if numbers are lucky as specified
+			String answer = "";
+			
+			for(int i : luckyNums) {
+				luckySum += i;
+			}
+			
+			int remainder = luckySum % 3;
+			
+			if(remainder == 0) {
+				answer = "You are very lucky";
+			} else if(remainder == 2) {
+				answer = "You are pretty lucky";
+			} else {
+				answer = "You are unlucky";
+			}
+			
+			
+			System.out.print("Lucky numbers: "); // formats answer in output as specified
+			
+			for(int i : luckyNums) {
+				System.out.print(i + " ");
+			}
+			
+			System.out.println("\n" + answer);
 			
 		}
 		
@@ -35,77 +78,77 @@ public class RandomGenerator {
 		System.out.println("Goodbye"); // end to program, executed after break is triggered
 		
 	}
-	
-	
-	private static String getName(Scanner input) {
-		System.out.println("What is Your Name?: ");
-		String name = input.nextLine(); //  scans whole line for all text input
-		return name;
-	}
-	
-	
-	private static int sumOfChars(String name) {
-		int sumOfChars = 0;
-		
-		for(int i = 0; i < name.length(); i++) {
-			sumOfChars += name.charAt(i); // sums numerical values of chars together
-		}
-		
-		return sumOfChars;
-		
-	}
-	
-	
-	private static int[] luckyNumGenerator(int i) {
-		
-		int[] luckyNums = new int[5];
-		int place = 0;
-		
-		while(place < 5) {
-			i = (73 * i + 37) % 128;
-			if(i <= 100) { //  ignores numbers over 100 and continues to next iteration of loop
-				luckyNums[place] = i; // uses while loop to ensure places reaches 4
-				place += 1;
-			}
-		}
-		
-		return luckyNums;
-	}
-	
-	
-	
-	private static String areTheyLucky(int[] luckyNums) {
-		
-		int luckySum = 0; // determines if numbers are lucky as specified
-		String answer = "";
-		
-		for(int i : luckyNums) {
-			luckySum += i;
-		}
-		
-		int remainder = luckySum % 3;
-		
-		if(remainder == 0) {
-			answer = "You are very lucky";
-		} else if(remainder == 2) {
-			answer = "You are pretty lucky";
-		} else {
-			answer = "You are unlucky";
-		}
-		
-		return answer;
-	}
-	
-	private static void printAnswer(int[]luckyNums, String luckStatus ) {
-		
-		System.out.print("Lucky numbers: "); // formats answer in output as specified
-		
-		for(int i : luckyNums) {
-			System.out.print(i + " ");
-		}
-		
-		System.out.println("\n" + luckStatus);
-	}
+//	can i have more than one method please
+//	
+//	private static String getName(Scanner input) {
+//		System.out.println("What is Your Name?: ");
+//		String name = input.nextLine(); //  scans whole line for all text input
+//		return name;
+//	}
+//	
+//	
+//	private static int sumOfChars(String name) {
+//		int sumOfChars = 0;
+//		
+//		for(int i = 0; i < name.length(); i++) {
+//			sumOfChars += name.charAt(i); // sums numerical values of chars together
+//		}
+//		
+//		return sumOfChars;
+//		
+//	}
+//	
+//	
+//	private static int[] luckyNumGenerator(int i) {
+//		
+//		int[] luckyNums = new int[5];
+//		int place = 0;
+//		
+//		while(place < 5) {
+//			i = (73 * i + 37) % 128;
+//			if(i <= 100) { //  ignores numbers over 100 and continues to next iteration of loop
+//				luckyNums[place] = i; // uses while loop to ensure places reaches 4
+//				place += 1;
+//			}
+//		}
+//		
+//		return luckyNums;
+//	}
+//	
+//	
+//	
+//	private static String areTheyLucky(int[] luckyNums) {
+//		
+//		int luckySum = 0; // determines if numbers are lucky as specified
+//		String answer = "";
+//		
+//		for(int i : luckyNums) {
+//			luckySum += i;
+//		}
+//		
+//		int remainder = luckySum % 3;
+//		
+//		if(remainder == 0) {
+//			answer = "You are very lucky";
+//		} else if(remainder == 2) {
+//			answer = "You are pretty lucky";
+//		} else {
+//			answer = "You are unlucky";
+//		}
+//		
+//		return answer;
+//	}
+//	
+//	private static void printAnswer(int[]luckyNums, String luckStatus ) {
+//		
+//		System.out.print("Lucky numbers: "); // formats answer in output as specified
+//		
+//		for(int i : luckyNums) {
+//			System.out.print(i + " ");
+//		}
+//		
+//		System.out.println("\n" + luckStatus);
+//	}
 
 	
 }
--- Homework/Assignment3/src/assign03/GradeCalculator.java
+++ Homework/Assignment3/src/assign03/GradeCalculator.java
@@ -1,121 +1,159 @@
 package assign03;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 public class GradeCalculator {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("input the path to file containing grades: ");
-		
-		String fileLocation = userInput.next();
-		
-		File grades = new File(fileLocation);
+
 		try {
 			Scanner fileInput = new Scanner(grades);
-			
+
 			double avgExam = fileInput.nextDouble();
 			double avgLab = fileInput.nextDouble();
 			double avgQuiz = fileInput.nextDouble();
 			int numAssignments = fileInput.nextInt();
+
 			int assignmentGradeSum = 0;
 			int highestAssignment = 0;
 			int lowestAssignment = 101;
 			int numZeroAssignments = 0;
 			String letterGrade = "";
 			String zeroAssignments = "";
-			
-			
-			
-			int [] assignmentGrades = new int[numAssignments];
-			
-			for(int i = 0; i < numAssignments; i++) {
+
+			int[] assignmentGrades = new int[numAssignments];
+
+			for (int i = 0; i < numAssignments; i++) {
 				assignmentGrades[i] = fileInput.nextInt();
 			}
-			
-			for(int i : assignmentGrades) {
+
+			for (int i : assignmentGrades) {
 				assignmentGradeSum += i;
-				if(i == 0) {
+				if (i == 0) {
 					zeroAssignments = zeroAssignments.concat(numZeroAssignments + " ");
 				}
-				
-				if(i > highestAssignment) {
+
+				if (i > highestAssignment) {
 					highestAssignment = i;
 				}
-				
-				if(i < lowestAssignment) {
+
+				if (i < lowestAssignment) {
 					lowestAssignment = i;
 				}
-				
+
 				numZeroAssignments += 1;
 			}
-			
-			double avgAssignment = (double)assignmentGradeSum/numAssignments;
-			
+
+			double avgAssignment = (double) assignmentGradeSum / numAssignments;
+
 			double totalGrade = (0.45 * avgExam) + (0.35 * avgAssignment) + (0.1 * avgLab) + (0.1 * avgQuiz);
-			
-			int tens = (int)totalGrade/10;
-			int ones = (int)totalGrade%10;
-			
-			if(tens < 6) {
+
+			int tens = (int) totalGrade / 10;
+			int ones = (int) totalGrade % 10;
+
+			if (tens < 6) {
 				letterGrade = "E";
-			}
-			else if(tens >= 9) {
+			} else if (tens >= 9) {
 				letterGrade = "A";
-			}
-			else if(tens >= 8) {
+			} else if (tens >= 8) {
 				letterGrade = "B";
-			}
-			else if(tens >= 7) {
+			} else if (tens >= 7) {
 				letterGrade = "C";
-			}
-			else if(tens >= 6) {
+			} else if (tens >= 6) {
 				letterGrade = "D";
 			}
-			
-			if(ones >= 0 && ones < 3) {
+
+			if (ones >= 0 && ones < 3) {
 				letterGrade = letterGrade.concat("-");
-			}
-			else if(ones >= 7 && ones < 10) {
+			} else if (ones >= 7 && ones < 10) {
 				letterGrade = letterGrade.concat("+");
 			}
-		
-			
-			
-	
+
 			String[] statements = createStatements();
-			
-			String finalGrades = String.format("%.2f", avgAssignment + '\n' + zeroAssignments + '\n' + highestAssignment 
+
+			String finalGrades = String.format("%.2f", avgAssignment + '\n' + zeroAssignments + '\n' + highestAssignment
 					+ "\n" + lowestAssignment + '\n' + totalGrade + '\n' + letterGrade + '\n');
-			
+
 			int beginningIndex = 0;
-			
+
 			String finalString = "";
-			
+
 			String currentPrint = "";
-			
-			for(int i = 0; i <= 5; i++) {
+
+			for (int i = 0; i <= 5; i++) {
 				finalString = finalString.concat(statements[i] + ": ");
 				int nextIndex = finalGrades.indexOf('\n') + 1;
 				currentPrint = finalGrades.substring(beginningIndex, nextIndex);
 				finalString = finalString.concat(currentPrint);
 				finalGrades = finalGrades.substring(nextIndex);
 			}
-			
+
 			System.out.print(finalString);
-			
-			userInput.close();
 			fileInput.close();
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.out.println("File not found. Please check the path and try again.");
 		}
+
+	}
+
+	public class gradeSummary{
+		double avgExam;
+		double avgLab;
+		double avgQuiz;
+		double avgAssignment;
+		int [] assignmentGrades;
+		double finalGrade;
+		
+		public gradeSummary() {
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("input the path to file containing grades: ");
+			
+			String fileLocation = userInput.next();
+			
+			File grades = new File(fileLocation);
+			userInput.close();
+			
+			try {
+				Scanner fileInput = new Scanner(grades);
+				
+				avgExam = fileInput.nextDouble();
+				avgLab = fileInput.nextDouble();
+				avgQuiz = fileInput.nextDouble();
+				int numAssignments = fileInput.nextInt();
+				
+				assignmentGrades = new int[numAssignments];
+				
+				for(int i = 0; i < assignmentGrades.length; i++) {
+					assignmentGrades[i] = fileInput.nextInt();
+				}
+				
+				fileInput.close();
+				} catch(FileNotFoundException e) {
+					System.out.println("File not found. Please check the path and try again.");
+			}
+		}
+		
+		public void calcAssignmentGrade(int[] assignments) {
+			int sum = 0;
+			
+			for (int i : assignments) {
+				sum += i;
+				}
+			double avg = (double) sum / assignments.length;
+			
+			avgAssignment = avg;
+		}
+		
+		public double calcFinalGrade() {
+			finalGrade = (0.45 * avgExam) + (0.35 * avgAssignment) + (0.1 * avgLab) + (0.1 * avgQuiz);
+			return finalGrade;
+		}
+		
 		
 	}
-	
-	
+
 	
 	public static final String[] createStatements() {
 		String[] statements = new String[6];
@@ -126,6 +164,8 @@ public class GradeCalculator {
 		statements[4] = "Course grade (numeric)";
 		statements[5] = "Course grade (letter)";
 		return statements;
-	}
-
+		}
+	
 }
+
+
