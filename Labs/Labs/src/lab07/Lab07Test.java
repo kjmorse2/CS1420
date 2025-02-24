0e6f36827a0e4bd2d80e78605ef223ad0e1dc73b
kjmorse2 <kenmorse2@proton.me>
2025-02-24 14:38:30 -0700

    clean up, all maven

---
+++ Labs/Labs/src/lab07/Lab07Test.java
@@ -0,0 +1,14 @@
+package lab07;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Lab07Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
